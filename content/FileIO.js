b73954f5adb8a8a9ed25894279e9fb842cdf6954
Scott Settle <scottbsettle@gmail.com>
2017-02-09 13:49:09 -0500

    cleaned up FileIO log function

--- content/FileIO.js
+++ content/FileIO.js
@@ -1,19 +1,16 @@
 function logAction(name, data){
 	var fs = require('fs');
-			  fs.open('LogFile', 'wx', (err, fd) => {
-               // if (err) {
-					// throw err;
-			// }
-				var outdata = "";
-				//fs.appendFile('LogFile', 'data to append', 'utf8', callback);
-				if (name === "~pickblock"){
-					outdata = data.id;				}else{
-				outdata = data.name;
-			}fs.appendFile('LogFile', new Date().toLocaleString() + " " + name + " " + outdata + "\n", (err) => {
-					if (err) throw err;
-				console.log('The "data to append" was appended to file!');
-					});
-				});
+	fs.open('LogFile', 'wx', (err, fd) => { 
+    var outdata = "";
+	if (name === "~pickblock"){
+		outdata = data.id;}
+		else{
+			outdata = data.name;
+		   }fs.appendFile('LogFile', new Date().toLocaleString() + " " + name + " " + outdata + "\n", (err) =>{
+			if (err) throw err;
+			//console.log('The "data to append" was appended to file!');
+			});
+	});
 }
 
 function logCodeExecute(c){
@@ -21,7 +18,7 @@ function logCodeExecute(c){
 	fs.appendFile('LogFile', new Date().toLocaleString() + " " + c + "\n", (err) => {
 		if (err)
 			throw err;
-		console.log('The "data to append" was appended to file!');
+		//console.log('The "data to append" was appended to file!');
 	});
 }
 
